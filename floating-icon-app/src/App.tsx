4974d0f6cc8ded06e7ed26d642d0c5dac99dce1e
rit-123 <diamondfox2264@gmail.com>
2025-11-22 14:55:30 -0600

    feat: Initial app setup with floating icon

---
+++ floating-icon-app/src/App.tsx
@@ -0,0 +1,13 @@
+import electronLogo from '/electron-vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="draggable-icon">
+      <img src={electronLogo} className="logo" alt="Electron logo" />
+    </div>
+  )
+}
+
+export default App
+
